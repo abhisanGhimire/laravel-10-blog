a673d9d50fa96c08521136eed9c7dfce4d22a161
Zura Sekhniashvili <sekhniashvilizura@gmail.com>
2023-04-02 09:05:59 +0400

    Start working on home page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,7 @@ use App\Models\Post;
 use App\Models\PostView;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -17,11 +18,62 @@ class PostController extends Controller
      */
     public function index(): View
     {
-        $posts = Post::query()
-            ->where('active', '=', 1)
-            ->whereDate('published_at', '<', Carbon::now())
-            ->orderBy('published_at', 'desc')
-            ->paginate(10);
+        // Latest post
+//        $latestPost = Post::query()
+//            ->where('active', '=', 1)
+//            ->whereDate('published_at', '<', Carbon::now())
+//            ->orderBy('published_at', 'desc')
+//            ->limit(1)
+//            ->first();
+
+        // Show the most popular 3 posts
+//        $posts = DB::table('posts')
+//            ->leftJoin('upvote_downvotes', 'posts.id', '=', 'upvote_downvotes.post_id')
+//            ->select('posts.*', DB::raw('COUNT(upvote_downvotes.id) as upvote_count'))
+//            ->where(function ($query) {
+//                $query->whereNull('upvote_downvotes.is_upvote')
+//                    ->orWhere('upvote_downvotes.is_upvote', '=', 1);
+//            })
+//            ->where('active', '=', 1)
+//            ->whereDate('published_at', '<', Carbon::now())
+//            ->groupBy('posts.id')
+//            ->orderByDesc('upvote_count')
+//            ->limit(3)
+//            ->get();
+
+        // Show recommended articles based on user likes and views
+        $userId = auth()->user()->id;
+
+        $userId = 1;
+
+        $posts = DB::table('posts')
+            ->leftJoin('category_post as cp', 'posts.id', '=', 'cp.post_id')
+            ->leftJoin(DB::raw('(SELECT cp.category_id, cp.post_id FROM upvote_downvotes
+                        JOIN category_post cp ON upvote_downvotes.post_id = cp.post_id
+                        WHERE upvote_downvotes.is_upvote = 1 AND upvote_downvotes.user_id = ?) AS t'), function ($join) {
+                $join->on('t.category_id', '=', 'cp.category_id')
+                    ->on('t.post_id', '<>', 'cp.post_id');
+            })
+            ->select('posts.*')
+            ->where('posts.id', '<>', DB::raw('t.post_id'))
+            ->setBindings([$userId])
+            ->get();
+
+
+        // Show top 5 categories with recent 3 articles
+        $posts = DB::table('posts')
+            ->leftJoin('upvote_downvotes', 'posts.id', '=', 'upvote_downvotes.post_id')
+            ->select('posts.*', DB::raw('COUNT(upvote_downvotes.id) as upvote_count'))
+            ->where(function ($query) {
+                $query->whereNull('upvote_downvotes.is_upvote')
+                    ->orWhere('upvote_downvotes.is_upvote', '=', 1);
+            })
+            ->groupBy('posts.id')
+            ->orderByDesc('upvote_count')
+            ->limit(5)
+            ->get();
+
+        dd($posts);
 
         return view('home', compact('posts'));
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout meta-title="TheCodeholic Blog" meta-description="Lorem ipsum dolor sit amet, consectetur adipisicing elit">
     <div class="container mx-auto flex flex-wrap py-6">
 
+
+
         <!-- Posts Section -->
         <section class="w-full md:w-2/3  px-3">
             <div class=" flex flex-col items-center">
